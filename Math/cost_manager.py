e313f36506139e54b2333f2c9ee21589329ca729
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2024-03-31 15:57:37 -0300

    Program in correct work.

--- Math/cost_manager.py
+++ Math/cost_manager.py
@@ -22,14 +22,14 @@ def cost_manager():
     }
     
     # Value of each item
-    water = float(input(f'What the of                {costs_fixed[1]}: '))
+    water = float(input(f'What the of {costs_fixed[1]}: '))
     eletric_energy = float(input(f'What the value of {costs_fixed[2]}: '))
-    internet = float(input(f'What the value of       {costs_fixed[3]}: '))
-    phone = float(input(f'What the value of          {costs_fixed[4]}: '))
-    graduation = float(input(f'What the value of     {costs_fixed[5]}: '))
-    car = float(input(f'What the value of            {costs_fixed[6]}: '))
-    rent = float(input(f'What the value of           {costs_fixed[7]}: '))
-    gasoline = float(input(f'What the value of       {costs_fixed[8]}: '))
+    internet = float(input(f'What the value of {costs_fixed[3]}: '))
+    phone = float(input(f'What the value of {costs_fixed[4]}: '))
+    graduation = float(input(f'What the value of {costs_fixed[5]}: '))
+    car = float(input(f'What the value of {costs_fixed[6]}: '))
+    rent = float(input(f'What the value of {costs_fixed[7]}: '))
+    gasoline = float(input(f'What the value of {costs_fixed[8]}: '))
 
     # Calc the some and media of results
     total_cost = water + eletric_energy + internet + phone + graduation + car + rent + gasoline
@@ -38,17 +38,17 @@ def cost_manager():
     
     # Print all results 
     print(f'\n          Total costs'.upper())
-    print(f'{costs_fixed[1]}:          R${water:.2f}')
-    print(f'{costs_fixed[2]}: R${eletric_energy:.2f}')
-    print(f'{costs_fixed[3]}:       R${internet:.2f}')
-    print(f'{costs_fixed[4]}:          R${phone:.2f}')
-    print(f'{costs_fixed[5]}:     R${graduation:.2f}')
-    print(f'{costs_fixed[6]}:            R${car:.2f}')
-    print(f'{costs_fixed[7]}:           R${rent:.2f}')
-    print(f'{costs_fixed[8]}:       R${gasoline:.2f}')
+    print(f'{costs_fixed[1]}:               R${water:.2f}')
+    print(f'{costs_fixed[2]}:      R${eletric_energy:.2f}')
+    print(f'{costs_fixed[3]}:            R${internet:.2f}')
+    print(f'{costs_fixed[4]}:               R${phone:.2f}')
+    print(f'{costs_fixed[5]}:          R${graduation:.2f}')
+    print(f'{costs_fixed[6]}:                 R${car:.2f}')
+    print(f'{costs_fixed[7]}:                R${rent:.2f}')
+    print(f'{costs_fixed[8]}:            R${gasoline:.2f}')
 
-    print(f'\nTotal costs:                   R${total_cost:.2f}')
-    print(f'Media of costs:                R${average_cost:.2f}')
-    print(f'Reste value after of payment: R${remove_values:.2f}')
+    print(f'\nTotal costs:                     R${total_cost:.2f}')
+    print(f'Media of costs:                    R${average_cost:.2f}')
+    print(f'Reste value after of payment:      R${remove_values:.2f}')
 
 cost_manager()
